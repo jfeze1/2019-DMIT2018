725eba01be6877ada3c510bfd6a90c86e00c33a7
jfeze1 <jfeze1@studentmail.nait.ca>
2019-05-06 21:46:39 -0600

    Created Artist entity

---
+++ WebApp/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    //Default class permission is private
+    //Annotation to link this class to the sql table
+    [Table("Artits")]
+    public class Artist
+    {
+        private string _Name;
+        [Key]
+        public int ArtistId { get; set; } // This is an automated implemented property
+
+        // Because Name is a nullable we make sure we handle it in our class by creating a private property _Name
+        //Always Fully implement nullable string property
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    _Name = null; 
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+
+    }
+}
